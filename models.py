d3b3426d1d42a88d094c50b80c9e3e1422cc2daa
lyne cha <lynecha@berkeley.edu>
2022-03-30 12:16:11 -0700

    added html templates for each page

--- models.py
+++ models.py
@@ -1,1 +1,26 @@
 """Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """ User Class """
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    first_name = db.Column(db.String(20),
+                    nullable=False)
+    last_name = db.Column(db.String(20),
+                    nullable=False)
+    image_url = db.Column(db.Text,
+                    nullable=True, default="https://kansai-resilience-forum.jp/wp-content/uploads/2019/02/IAFOR-Blank-Avatar-Image-1.jpg")
